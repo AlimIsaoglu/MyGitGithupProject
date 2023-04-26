9ea29e0bc02e608cdba04130988865797bfee17a
Ali <ali.isaoglu@gmail.com>
2023-04-26 11:01:47 +0200

    v1

---
+++ src/AtmApp.java
@@ -0,0 +1,15 @@
+public class AtmApp {
+    public static void main(String[] args) {
+
+        System.out.println("Bu bir ATM projesidir");
+        //Version1
+
+        System.out.println("Java is easy");
+        //Version 2
+
+
+
+
+
+    }//main
+}//class
